0d5981ba7a6631dc31e7aafde13d72ae64ec8f0f
jezman <dev.jezman@gmail.com>
2018-12-25 16:57:19 +0300

    Add debuging print.

--- include/main.h
+++ include/main.h
@@ -4,17 +4,24 @@
 
 #define DEBUG 0
 
-// #define PIXEL_PIN 4
-// #define PIXEL_NUM 16
-// #define ENCODER_A_PIN 2
-// #define ENCODER_B_PIN 3
-// #define ENCODER_BUTTON_PIN 7
+#if DEBUG
+#define PRINTS(s)   { Serial.print(F(s)); }
+#define PRINT(s,v)  { Serial.print(F(s)); Serial.print(v); }
+#else
+#define PRINTS(s)
+#define PRINT(s,v)
+#endif
+
+#define PIXEL_PIN 4
+#define PIXEL_NUM 16
+#define ENCODER_A_PIN 2
+#define ENCODER_B_PIN 3
+#define ENCODER_BUTTON_PIN 7
+
 // #define SERVO_PIN 10
 // #define SERVO_CLOSED_MICROS 1800
 // #define SERVO_OPEN_MICROS 1800
 
-void PinA(void);
-void PinB(void);
 void encoderPinAHandler(void);
 void encoderPinBHandler(void);
 void updateFlower(void);
